54d788e8f7e13dade51c42e3a88b8996101ef6cb
francesco-strazzullo <me@francescostrazzullo.info>
2016-10-14 17:43:38 +0200

    managed input

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
-import virtualDom from 'virtual-dom';
+import {h,diff,patch,create} from 'virtual-dom';
 
-console.log(virtualDom);
+let left = 0;
+let top = 0;
+
+const move = () => {
+    const newTree = render({left,top});
+    const patches = diff(tree, newTree);
+    rootNode = patch(rootNode, patches);
+    tree = newTree;
+};
+
+const onInputChange = (event) => {
+    left = parseInt(event.target.value,10);
+};
+
+const render = ({left,top}) => {
+
+    const input = h('input', {
+        type: 'text',
+        value: left,
+        oninput:onInputChange
+    });
+
+    return h('div', {
+        style: {
+            textAlign: 'center',
+            lineHeight: '100px',
+            border: '1px solid red',
+            width: '100px',
+            height: '100px',
+            position: 'fixed',
+            left: left + 'px',
+            top: top + 'px'
+        },
+        onclick: move
+    },['Click to move',input]);
+};
+
+let tree = render({left,top});
+
+let rootNode = create(tree);
+
+document.body.appendChild(rootNode);
